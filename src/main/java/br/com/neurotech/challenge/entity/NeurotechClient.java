e1f46a884c45242b3f5bee3a243ef9b123611f64
matheusphalves <matheusphalves@gmail.com>
2024-04-18 21:42:57 -0300

    feat: Setup NeurotechClient entity

--- src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
+++ src/main/java/br/com/neurotech/challenge/entity/NeurotechClient.java
@@ -1,12 +1,25 @@
 package br.com.neurotech.challenge.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
-@Data
-public class NeurotechClient {
-	
-	private String name;
-	private Integer age;
-	private Double income;
+import java.io.Serializable;
+import java.util.UUID;
 
+@Data
+@Entity
+@Table(name = "NEUROTECHCLIENT")
+public class NeurotechClient implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String name;
+    private Integer age;
+    private Double income;
 }
