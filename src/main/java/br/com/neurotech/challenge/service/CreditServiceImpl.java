6cb96232597603a7d2e3635c42293cb5dcda5535
matheusphalves <matheusphalves@gmail.com>
2024-04-18 21:51:04 -0300

    feat: Implement credit service

---
+++ src/main/java/br/com/neurotech/challenge/service/CreditServiceImpl.java
@@ -0,0 +1,46 @@
+package br.com.neurotech.challenge.service;
+
+import br.com.neurotech.challenge.entity.NeurotechClient;
+import br.com.neurotech.challenge.entity.VehicleModel;
+import org.springframework.stereotype.Service;
+
+import static br.com.neurotech.challenge.constant.CreditConstants.HATCH_MAX_INCOME;
+import static br.com.neurotech.challenge.constant.CreditConstants.HATCH_MIN_INCOME;
+import static br.com.neurotech.challenge.constant.CreditConstants.SUV_AGE_OLDER;
+import static br.com.neurotech.challenge.constant.CreditConstants.SUV_INCOME_GREATER;
+
+@Service
+public class CreditServiceImpl implements CreditService {
+
+    private final ClientService clientService;
+
+    public CreditServiceImpl(ClientService clientService) {
+
+        this.clientService = clientService;
+    }
+
+    @Override
+    public boolean checkCredit(String clientId, VehicleModel model) {
+
+        NeurotechClient neurotechClient = clientService.get(clientId);
+
+        if (model.equals(VehicleModel.HATCH))
+            return checkHATCHCredit(neurotechClient);
+
+        return checkSUVCredit(neurotechClient);
+    }
+
+    private boolean checkHATCHCredit(NeurotechClient neurotechClient) {
+
+        double income = neurotechClient.getIncome();
+
+        return income >= HATCH_MIN_INCOME && income <= HATCH_MAX_INCOME;
+    }
+
+    private boolean checkSUVCredit(NeurotechClient neurotechClient) {
+
+        return neurotechClient.getIncome() > SUV_INCOME_GREATER
+                && neurotechClient.getAge() > SUV_AGE_OLDER;
+
+    }
+}
