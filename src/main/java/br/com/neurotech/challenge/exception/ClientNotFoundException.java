bcb72503f445cb2284d685bd6bc322c883ddbc6e
matheusphalves <matheusphalves@gmail.com>
2024-04-18 21:50:06 -0300

    feat: Add exception to handle with client find

---
+++ src/main/java/br/com/neurotech/challenge/exception/ClientNotFoundException.java
@@ -0,0 +1,9 @@
+package br.com.neurotech.challenge.exception;
+
+public class ClientNotFoundException extends RuntimeException {
+
+    public ClientNotFoundException(String message) {
+        super(message);
+    }
+
+}
