5cf9af10da9e966cbfffbcf24fc6664f07ba155d
matheusphalves <matheusphalves@gmail.com>
2024-04-18 21:48:57 -0300

    feat: Add some constants for credit checking

---
+++ src/main/java/br/com/neurotech/challenge/constant/CreditConstants.java
@@ -0,0 +1,12 @@
+package br.com.neurotech.challenge.constant;
+
+public class CreditConstants {
+
+
+    public static final double HATCH_MIN_INCOME = 5000;
+    public static final double HATCH_MAX_INCOME = 15000;
+
+    public static final double SUV_INCOME_GREATER = 8000;
+    public static final int SUV_AGE_OLDER = 15000;
+
+}
