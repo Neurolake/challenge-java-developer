0e1d7a874704fba40e4e6689000112e3607974f0
matheusphalves <matheusphalves@gmail.com>
2024-04-20 10:21:08 -0300

    docs: Add openapi definition

--- src/main/java/br/com/neurotech/challenge/ChallengeJavaDeveloperApplication.java
+++ src/main/java/br/com/neurotech/challenge/ChallengeJavaDeveloperApplication.java
@@ -1,9 +1,12 @@
 package br.com.neurotech.challenge;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Neurotech client API", version = "1.0", description = "Client and credit availability information"))
 public class ChallengeJavaDeveloperApplication {
 
 	public static void main(String[] args) {
