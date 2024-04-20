63182a709f702db2ecd8ceffdf85a8e9094eaf45
matheusphalves <matheusphalves@gmail.com>
2024-04-20 18:38:06 -0300

    feat: Add some validations to register dto

--- src/main/java/br/com/neurotech/challenge/dto/NeurotechClientRegisterDto.java
+++ src/main/java/br/com/neurotech/challenge/dto/NeurotechClientRegisterDto.java
@@ -1,7 +1,19 @@
 package br.com.neurotech.challenge.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
 public record NeurotechClientRegisterDto(
+
+        @NotBlank(message = "The field name cannot be blank")
         String name,
+
+        @Positive(message = "The age should be a positive number")
+        @NotNull(message = "The field age cannot be null")
         Integer age,
+
+        @Positive(message = "The age should be a positive number")
+        @NotNull(message = "The field age cannot be null")
         Double income) {
 }
