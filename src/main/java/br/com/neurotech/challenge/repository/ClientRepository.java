2542afad579daf4f1b609d73aa71b8f9722db1fa
matheusphalves <matheusphalves@gmail.com>
2024-04-19 21:55:19 -0300

    feat: Add find by name method

--- src/main/java/br/com/neurotech/challenge/repository/ClientRepository.java
+++ src/main/java/br/com/neurotech/challenge/repository/ClientRepository.java
@@ -4,9 +4,11 @@ import br.com.neurotech.challenge.entity.NeurotechClient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ClientRepository extends JpaRepository<NeurotechClient, UUID> {
 
+    Optional<NeurotechClient> findByName(String name);
 }
