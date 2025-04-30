dc7a103d7b932c5f21749d118f9c4fd291126258
Francisco Costa <franciscocosta@Mac.local>
2025-04-29 23:02:49 -0300

    test: add unit tests for InMemoryClientRepository

--- src/test/java/br/com/neurotech/challenge/repository/InMemoryClientRepositoryTest.java
+++ src/test/java/br/com/neurotech/challenge/repository/InMemoryClientRepositoryTest.java
@@ -2,7 +2,6 @@ package br.com.neurotech.challenge.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
